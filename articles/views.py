b685e682cc131c5ecf9ca3dca9e1aa2034e457a0
HARRY <hamidreza.amiri800@gmail.com>
2024-08-12 14:31:35 +0330

    commited

--- articles/views.py
+++ articles/views.py
@@ -49,6 +49,9 @@ def article_detail_view(request, slug=None):
 
 
 def article_image_upload_view(request, parent_id=None):
+    template_name = "articles/image-article-upload-form.html"
+    if request.htmx:
+        template_name = "articles/image-article-upload.html"
     try:
         parent_obj = Article.objects.get(id=parent_id, user=request.user)
     except:
@@ -58,6 +61,6 @@ def article_image_upload_view(request, parent_id=None):
     form = ArticleImageForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         obj = form.save(commit=False)
-        obj.article = parent_obj 
+        obj.article = parent_obj
         obj.save()
-    return render(request, "image-form.html", {"form": form})
+    return render(request, template_name, {"form": form})
